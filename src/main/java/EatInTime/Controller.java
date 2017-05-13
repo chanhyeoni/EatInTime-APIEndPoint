136f40a8acddf4dc85b32c0dc9a5f6dc91b40583
CHL <chan.hyeo.ni.3698@gmail.com>
2017-05-13 17:07:55 -0400

    testing the insert function

--- src/main/java/EatInTime/Controller.java
+++ src/main/java/EatInTime/Controller.java
@@ -98,10 +98,10 @@ public class Controller {
 	* - the error message if the insertion fails
 	**/
 	@RequestMapping(value="/insertNewData", method=RequestMethod.POST)
-	public @ResponseBody String insertNewData(@RequestParam(value="inputLine", required=true) JsonData inputLine){
+	public @ResponseBody void insertNewData(@RequestParam(value="inputLine", required=true) JsonData inputLine){
 		inputLine.print();
 		//String successMsg = dbObjMongoDb.insert(inputLine);
-		return inputLine;
+		//return inputLine;
 	}
 
 
